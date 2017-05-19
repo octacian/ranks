c568c3a793e057b4726847efe0a4dd92c0060579
octacian <enduffy2014@outlook.com>
2017-05-19 08:37:08 -0700

    Initial commit

---
+++ ranks.lua
@@ -0,0 +1,21 @@
+-- ranks/ranks.lua
+
+ranks.register("admin", {
+	prefix = "Admin",
+	colour = {a = 255, r = 230, g = 33, b = 23},
+})
+
+ranks.register("moderator", {
+	prefix = "Moderator",
+	colour = {a = 255, r = 255, g = 83, b = 37},
+})
+
+ranks.register("guardian", {
+	prefix = "Guardian",
+	colour = {a = 255, r = 255, g = 132, b = 0},
+})
+
+ranks.register("youtube", {
+	prefix = "YouTube",
+	colour = {a = 255, r = 255, g = 80, b = 71},
+})
