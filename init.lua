e99643709fa7a051176347b50fb5aa7f4bc6b3e5
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2022-05-26 16:04:57 +0200

    Removed prefix from irc
    Because there is no good way to add a prefix before the name without changing the irc.playerMessage function.

--- init.lua
+++ init.lua
@@ -248,7 +248,7 @@ function ranks.chat_send(name, message)
 					        if nl then
 					                message = message:sub(1, nl - 1)
 					        end
-					        irc.say(irc.playerMessage(name, minetest.strip_colors("[" .. prefix .. "] " .. message)))
+					        irc.say(irc.playerMessage(name, minetest.strip_colors(message)))
 					end
 					minetest.chat_send_all(prefix.." <"..name.."> "..message)
 					minetest.log("action", "CHAT: ".."<"..name.."> "..message)
